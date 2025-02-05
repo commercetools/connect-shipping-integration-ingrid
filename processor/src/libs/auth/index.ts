8a2d5d087d3b9c2a69feae43c5ee7a9b67acd07d
Marius <73521657+mariusmhm@users.noreply.github.com>
2025-02-05 17:37:26 +0100

    misc(processor): again

--- processor/src/libs/auth/index.ts
+++ processor/src/libs/auth/index.ts
@@ -1,5 +1,5 @@
 export * from './authentications';
 export * from './hooks';
 export * from './services';
-export * from './SessionHeaderAuthInitializerx';
+export * from './';
 export * from './types';
