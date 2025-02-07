7de5a8632aedf9396b0af8cb3463fbc2c749ec62
King-Hin Leung <kinghin.leung@commercetools.com>
2025-02-07 11:32:31 +0100

    chore(session-finisher): init app

---
+++ session-finisher/src/connector/actions.ts
@@ -0,0 +1,72 @@
+import {
+  Destination,
+  GoogleCloudPubSubDestination,
+} from '@commercetools/platform-sdk';
+import { ByProjectKeyRequestBuilder } from '@commercetools/platform-sdk/dist/declarations/src/generated/client/by-project-key-request-builder';
+
+const ORDER_CREATE_SUBSCRIPTION_KEY =
+  'ingridShippingConnector-orderCreateSubscription';
+
+export async function createGcpPubSubOrderCreateSubscription(
+  apiRoot: ByProjectKeyRequestBuilder,
+  topicName: string,
+  projectId: string
+): Promise<void> {
+  const destination: GoogleCloudPubSubDestination = {
+    type: 'GoogleCloudPubSub',
+    topic: topicName,
+    projectId,
+  };
+  await createSubscription(apiRoot, destination);
+}
+
+async function createSubscription(
+  apiRoot: ByProjectKeyRequestBuilder,
+  destination: Destination
+) {
+  await deleteOrderCreateSubscription(apiRoot);
+  await apiRoot
+    .subscriptions()
+    .post({
+      body: {
+        key: ORDER_CREATE_SUBSCRIPTION_KEY,
+        destination,
+        messages: [
+          {
+            resourceTypeId: 'customer',
+            types: ['CustomerCreated'],
+          },
+        ],
+      },
+    })
+    .execute();
+}
+
+export async function deleteOrderCreateSubscription(
+  apiRoot: ByProjectKeyRequestBuilder
+): Promise<void> {
+  const {
+    body: { results: subscriptions },
+  } = await apiRoot
+    .subscriptions()
+    .get({
+      queryArgs: {
+        where: `key = "${ORDER_CREATE_SUBSCRIPTION_KEY}"`,
+      },
+    })
+    .execute();
+
+  if (subscriptions.length > 0) {
+    const subscription = subscriptions[0];
+
+    await apiRoot
+      .subscriptions()
+      .withKey({ key: ORDER_CREATE_SUBSCRIPTION_KEY })
+      .delete({
+        queryArgs: {
+          version: subscription.version,
+        },
+      })
+      .execute();
+  }
+}
