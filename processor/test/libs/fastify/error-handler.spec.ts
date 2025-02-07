ef6102db50d741a5bbd4884eeb1902c339df5385
King-Hin Leung <kinghin.leung@commercetools.com>
2025-02-07 10:27:36 +0100

    chore(processor): import requestConextPlugin from index

--- processor/test/libs/fastify/error-handler.spec.ts
+++ processor/test/libs/fastify/error-handler.spec.ts
@@ -2,7 +2,7 @@ import { afterEach, beforeEach, describe, expect, test } from '@jest/globals';
 import Fastify, { type FastifyInstance, FastifyError } from 'fastify';
 import { errorHandler } from '../../../src/libs/fastify/error-handler';
 import { CustomErrorAdditionalOpts } from '../../../src/libs/fastify/errors/dtos/error.dto';
-import { requestContextPlugin } from '../../../src/libs/fastify/context/context';
+import { requestContextPlugin } from '../../../src/libs/fastify/context';
 import { FastifySchemaValidationError } from 'fastify/types/schema';
 import { CustomError, ErrorAuthErrorResponse } from '../../../src/libs/fastify/errors';
 
