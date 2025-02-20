477a73dc33c95e153f041057621b069bebdcccc3
Joel Fickson <Joelngozo@gmail.com>
2025-02-20 13:19:27 +0100

    feat(ingrid): enhance event controller to integrate Ingrid API client for checkout session management

--- event-handler/src/controllers/event.controller.ts
+++ event-handler/src/controllers/event.controller.ts
@@ -1,8 +1,11 @@
 import { Request, Response } from 'express';
-import { createApiRoot } from '../client/create.client';
+import { createApiRoot } from '../client/commercetools/create.client';
 import CustomError from '../errors/custom.error';
 import { logger } from '../utils/logger.utils';
 import PubSubValidator from '../utils/validate_requsts.utils';
+import IngridApiClient from '../client/ingrid/ingrid.client';
+import { readConfiguration } from '../utils/config.utils';
+import type { IngridCompleteSessionRequestPayload } from '../client/ingrid/types/ingrid.client.type';
 
 /**
  * Exposed event POST endpoint.
@@ -21,43 +24,42 @@ export const post = async (request: Request, response: Response) => {
   const orderId = decodedData?.orderId;
 
   try {
-    const order = await createApiRoot()
+    const commercetoolsOrder = await createApiRoot()
       .orders()
       .withId({ ID: orderId })
-      .get()
-      .execute();
+      .get({
+        queryArgs: {
+          expand: 'cart',
+        },
+      })
+      .execute()
+      .then((res) => res.body);
 
-    // Execute the tasks in need
-    logger.info(order);
+    const order = commercetoolsOrder.cart?.id;
+
+    const ingridSessionId =
+      commercetoolsOrder.cart?.obj?.custom?.fields?.ingridSessionId;
+
+    const apiSecret = readConfiguration().ingridApiKey;
+    const environment = readConfiguration().ingridEnvironment;
+
+    const ingridOpts = {
+      apiSecret,
+      environment,
+    };
+
+    const ingridClient = new IngridApiClient(ingridOpts);
+
+    const payLoad: IngridCompleteSessionRequestPayload = {
+      checkout_session_id: ingridSessionId,
+      external_id: orderId,
+    };
+
+    const ingridResponse = await ingridClient.completeCheckoutSession(payLoad);
+
+    logger.info(ingridResponse);
+    return response.status(204).send();
   } catch (error) {
     throw new CustomError(400, `Bad request: ${error}`);
   }
-
-  // Return the response for the client
-  response.status(204).send();
-};
-
-export const update = async (request: Request, response: Response) => {
-  const body = PubSubValidator.validateRequestBody(request);
-  const message = PubSubValidator.validateMessageFormat(body);
-  const decodedData = PubSubValidator.decodeMessageData<{ orderId: string }>(
-    message
-  );
-  const orderId = decodedData?.orderId;
-
-  try {
-    const order = await createApiRoot()
-      .orders()
-      .withId({ ID: orderId })
-      .get()
-      .execute();
-
-    // Execute the tasks in need
-    logger.info(order);
-  } catch (error) {
-    throw new CustomError(400, `Bad request: ${error}`);
-  }
-
-  // Return the response for the client
-  response.status(204).send();
 };
