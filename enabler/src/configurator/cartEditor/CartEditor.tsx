0e59ea925646e3f244df3a5150885b9c5a2d1a36
King-Hin Leung <kinghin.leung@commercetools.com>
2025-02-21 11:05:44 +0100

    chore(enabler): clear textarea when delete cart

--- enabler/src/configurator/cartEditor/CartEditor.tsx
+++ enabler/src/configurator/cartEditor/CartEditor.tsx
@@ -1,10 +1,11 @@
-import { memo, useSyncExternalStore } from "react";
+import { memo, useSyncExternalStore, useState, useEffect} from "react";
 import cartStore from "../stores/cartStore";
 import cocoSessionStore from "../stores/cocoSessionStore";
 import { LocaleCountryCurrency } from "./LocaleCountryCurrency";
 import { ProductSearch } from "./ProductSearch";
 import loadingStore from "../stores/loadingStore";
 import countryCurrencyLanguageStore from "../stores/countryCurrencyLanguageStore";
+import '../css/CartEditor.css';
 
 function CartEditor() {
   const cart = useSyncExternalStore(cartStore.subscribe, cartStore.getSnapshot);
@@ -20,25 +21,37 @@ function CartEditor() {
     loadingStore.subscribe,
     loadingStore.getSnapshot
   );
+  const [textareaValue, setTextareaValue] = useState(JSON.stringify(cart, undefined, 2));
+
+  useEffect(() => {
+    setTextareaValue(JSON.stringify(cart, undefined, 2));
+  }, [cart]);
 
   return ccl.country && ccl.currency && ccl.language ? (
-    <div>
-      {session ? <p>SessionId: {session.id}</p> : ""}
-      {cart && (
-        <button
-          disabled={loading}
-          onClick={() =>
-            cartStore.dispatch({
-              type: "DELETE_CART",
-            })
-          }
-        >
-          Delete cart
-        </button>
-      )}
-      <LocaleCountryCurrency cart={cart} />
-      <ProductSearch />
-      <pre>{JSON.stringify(cart, undefined, 2)}</pre>
+    <div className="cart-editor">
+      <div className="cart-editor_column">
+          {session ? <p>SessionId: {session.id}</p> : ""}
+          {cart && (
+            <button
+              disabled={loading}
+              onClick={() => {
+                  setTextareaValue('');
+                  cartStore.dispatch({
+                    type: "DELETE_CART",
+                  })
+                }
+              }
+            >
+            Delete cart
+            </button>
+          )}
+          <LocaleCountryCurrency cart={cart} />
+          <ProductSearch />
+        </div>
+        <div className="cart-editor_column">
+          <p><b>Cart Object</b></p>
+          <textarea id="cart-editor_textarea-id" className="cart-editor_textarea"  cols={70} rows={40} value={textareaValue} />
+        </div>
     </div>
   ) : undefined;
 }
