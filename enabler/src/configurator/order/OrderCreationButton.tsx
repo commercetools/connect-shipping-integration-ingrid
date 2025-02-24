d474bf711afe57ee9e25742374e9b7bc63ccff29
Joel Fickson <Joelngozo@gmail.com>
2025-02-24 14:43:06 +0100

    fix(order): improve error handling and logging in OrderCreationButton component

--- enabler/src/configurator/order/OrderCreationButton.tsx
+++ enabler/src/configurator/order/OrderCreationButton.tsx
@@ -1,50 +1,52 @@
-import { memo, useEffect, useState, useSyncExternalStore } from "react";
-import cocoSessionStore from "../stores/cocoSessionStore";
-import client from "../coco";
-import { Cart } from "@commercetools/platform-sdk";
+import { memo, useEffect, useState, useSyncExternalStore } from 'react';
+import cocoSessionStore from '../stores/cocoSessionStore';
+import client from '../coco';
+import type { Cart } from '@commercetools/platform-sdk';
 
 const OrderCreationButton = memo(function OrderCreationButton() {
   const [orderCreation, setOrderCreation] = useState(false);
   const session = useSyncExternalStore(
     cocoSessionStore.subscribe,
-    cocoSessionStore.getSnapshot
+    cocoSessionStore.getSnapshot,
   );
 
   useEffect(() => {
-    const cartString = localStorage.getItem("cart");
-    const cart = cartString? JSON.parse(cartString) as Cart : undefined;
+    const cartString = localStorage.getItem('cart');
+    const cart = cartString ? JSON.parse(cartString) as Cart : undefined;
     if (orderCreation && cart) {
-      
+
       const cartId = cart.id;
       let cartVersion: number;
       setTimeout(() => {
-         
+
         client
           .carts().withId({ ID: cartId })
           .get()
           .execute()
           .then((result) => {
-            console.log("Cart feteched", result);
-            cartVersion = result?.body.version as number ;
-            return cartVersion
+            console.log('Cart feteched', result);
+            cartVersion = result?.body.version as number;
+            return cartVersion;
           }).then((cartVersion) => {
-              console.log("Create order from cart");
-              client
-                .orders()
-                .post({body:{
-                  "cart" : {
-                    "id" : cartId,
-                    "typeId" : "cart"
-                  },
-                  "version" : cartVersion
-                }})
-                .execute()
-          }).then((result) => {
-            console.log("Order created", result);
-          })
-          .catch((e) => console.error("something went wrong:", e));
+          console.log('Create order from cart');
+          client
+            .orders()
+            .post({
+              body: {
+                'cart': {
+                  'id': cartId,
+                  'typeId': 'cart',
+                },
+                'version': cartVersion,
+              },
+            })
+            .execute();
+        }).then((result) => {
+          console.log('Order created', result);
+        })
+          .catch((e) => console.error('something went wrong:', e));
       }, 500);
-      
+
     }
   }, [orderCreation]);
 
