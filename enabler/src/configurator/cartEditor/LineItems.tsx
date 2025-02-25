b34ac83fdb1523660f247aa035a25fcc311d6166
King-Hin Leung <kinghin.leung@commercetools.com>
2025-02-25 15:27:40 +0100

    chore(enabler): show discount breakdown in testing page

--- enabler/src/configurator/cartEditor/LineItems.tsx
+++ enabler/src/configurator/cartEditor/LineItems.tsx
@@ -1,9 +1,31 @@
 import { memo } from "react";
 
 import '../css/CartEditor.css';
-import type { Cart } from "@commercetools/platform-sdk";
+import type { Cart, LineItem } from "@commercetools/platform-sdk";
 
 type LineItemsProp = { cart: Cart | undefined};
+
+
+function getTotalProductDiscount(lineItem: LineItem) {
+  const productDiscountPerItem = lineItem.price.value.centAmount - (lineItem.price.discounted?.value.centAmount ?? lineItem.price.value.centAmount);
+  const totalProductDiscount = productDiscountPerItem * lineItem.quantity;
+  return totalProductDiscount
+}
+function getTotalLineItemDiscount(lineItem: LineItem) {
+  console.log(lineItem.discountedPricePerQuantity)
+  if (lineItem.discountedPricePerQuantity && lineItem.discountedPricePerQuantity.length>0) {
+    const totalDiscountedPrice = lineItem.discountedPricePerQuantity.reduce((acc, item) => {
+      return acc + item.discountedPrice.value.centAmount * item.quantity;
+    }, 0);
+    return (lineItem.price.discounted?.value.centAmount ?? lineItem.price.value.centAmount)  * lineItem.quantity - totalDiscountedPrice
+    
+  } 
+  return 0
+}
+
+function getTotalCartDiscount(lineItem: LineItem) {
+  return getTotalProductDiscount(lineItem) +  getTotalLineItemDiscount(lineItem)
+}
 
 
 export const LineItems = memo(function LineItems({
@@ -23,17 +45,25 @@ export const LineItems = memo(function LineItems({
       <table style={{ border: "1px solid black" }}>
         <thead>
           <tr>
-            <th style={{width: 150}} align="left">Name</th>
-            <th style={{width: 100}} align="left">Quantity</th>
-            <th style={{width: 150}} align="left">Tax included price</th>
-            <th style={{width: 150}} align="left">Tax</th>
+            <th style={{width: 100}} align="left">Name</th>
+            <th style={{width: 50}} align="left">Quantity</th>
+            <th style={{width: 100}} align="left">Original price</th>
+            <th style={{width: 100}} align="left">Tax included price</th>
+            <th style={{width: 50}} align="left">Tax</th>
+            <th style={{width: 100}} align="left">Total Product Discount</th>
+            <th style={{width: 100}} align="left">Total Line Item Discount</th>
+            <th style={{width: 100}} align="left">Total Cart Discount</th>
           </tr>
           {lineItems.map((item, index) => (
             <tr className="standard-font" key={index}>
-              <td style={{width: 150}} align="left">{item.name["en-US"]}</td>
-              <td style={{width: 100}} align="left">{item.quantity}</td>
-              <td style={{width: 150}} align="left"> {item.taxedPrice?.totalGross.centAmount}</td>
-              <td style={{width: 150}} align="left"> {item.taxedPrice?.totalTax?.centAmount}</td>
+              <td style={{width: 100}} align="left">{item.name["en-US"]}</td>
+              <td style={{width: 50}} align="left">{item.quantity}</td>
+              <td style={{width: 100}} align="left"> {item.price?.value.centAmount*item.quantity}</td>
+              <td style={{width: 100}} align="left"> {item.taxedPrice?.totalGross.centAmount}</td>
+              <td style={{width: 50}} align="left"> {item.taxedPrice?.totalTax?.centAmount}</td>
+              <td style={{width: 100}} align="left"> {getTotalProductDiscount(item)}</td>
+              <td style={{width: 100}} align="left"> {getTotalLineItemDiscount(item)}</td>
+              <td style={{width: 100}} align="left"> {getTotalCartDiscount(item)}</td>
             </tr>
           ))}
         </thead>
