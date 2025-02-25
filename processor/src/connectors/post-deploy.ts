35615c11dddb2fd8c97514f05c8b6abdaec08640
Marius Münstermann <73521657+mariusmhm@users.noreply.github.com>
2025-02-25 15:35:26 +0100

    chore(processor): Add post-deploy script for taxCategory and customType Handling (#63)
    * chore(processor): included tax category on env and config
    
    * chore(processor): post-deploy script for checking, updating, creating customType works
    
    * chore(processor): implemented taxCategory creation / validation on post-deploy script
    
    * fix(processor): linting
    
    * misc(processor): clean up last todo comments
    
    * chore(processor): marked context props as optional
    
    * fix(processor): adjusted error message on ingrid api client

--- processor/src/connectors/post-deploy.ts
+++ processor/src/connectors/post-deploy.ts
@@ -1,11 +1,48 @@
 import * as dotenv from 'dotenv';
+import { CommercetoolsApiClient } from '../clients/commercetools/api.client';
+import { appLogger } from '../libs/logger';
+import { handleCustomTypeAction, handleTaxCategoryAction } from './actions';
 
 dotenv.config();
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+/**
+ * Executes post-deployment setup tasks for the Ingrid connector
+ *
+ * @remarks
+ * This function performs two main tasks:
+ * 1. Sets up a custom type for storing Ingrid session IDs, if it doesn't exist
+ * 2. Creates a tax category specifically for Ingrid if it doesn't exist
+ *
+ * @param _properties - Map containing environment variables and configuration properties
+ * @param _properties.CTP_CLIENT_ID - Commercetools API client ID
+ * @param _properties.CTP_CLIENT_SECRET - Commercetools API client secret
+ * @param _properties.CTP_AUTH_URL - Commercetools auth URL
+ * @param _properties.CTP_API_URL - Commercetools API URL
+ * @param _properties.CTP_PROJECT_KEY - Commercetools project key
+ * @param _properties.INGRID_SESSION_CUSTOM_TYPE_KEY - Key for the Ingrid session custom type (defaults to 'ingrid-session')
+ * @param _properties.INGRID_SPECIFIC_TAX_CATEGORY_KEY - Key for the Ingrid tax category (defaults to 'ingrid-tax')
+ *
+ * @returns Promise that resolves when post-deployment tasks are complete
+ *
+ * @throws Will throw an error if either the custom type or tax category setup fails
+ */
 async function postDeploy(_properties: Map<string, unknown>) {
-  // TODO: as the TODO above the function. commented it out for now to pass the build process
-  //await checkIfIngridCustomTypeExists()
+  const client = new CommercetoolsApiClient({
+    clientId: _properties.get('CTP_CLIENT_ID') as string,
+    clientSecret: _properties.get('CTP_CLIENT_SECRET') as string,
+    authUrl: _properties.get('CTP_AUTH_URL') as string,
+    apiUrl: _properties.get('CTP_API_URL') as string,
+    projectKey: _properties.get('CTP_PROJECT_KEY') as string,
+    logger: appLogger,
+  });
+
+  const ingridCustomTypeKey = _properties.get('INGRID_SESSION_CUSTOM_TYPE_KEY') as string; // default: ingrid-session
+  const ingridTaxCategoryKey = _properties.get('INGRID_SPECIFIC_TAX_CATEGORY_KEY') as string; // default: ingrid-tax
+
+  await Promise.all([
+    handleCustomTypeAction(client, ingridCustomTypeKey),
+    handleTaxCategoryAction(client, ingridTaxCategoryKey),
+  ]);
 }
 
 async function run() {
@@ -19,62 +56,5 @@ async function run() {
     process.exitCode = 1;
   }
 }
-/* 
-// TODO: this needs to be refactored, currently as of development, logic is inside service
-export async function checkIfIngridCustomTypeExists() {
-  const sdk = new CommercetoolsApiClient({
-    clientId: process.env.CT_CLIENT_ID!,
-    clientSecret: process.env.CT_CLIENT_SECRET!,
-    authUrl: process.env.CT_AUTH_URL!,
-    apiUrl: process.env.CT_API_URL!,
-    projectKey: process.env.CT_PROJECT_KEY!,
-    sessionUrl: process.env.CT_SESSION_URL!,
-    jwksUrl: process.env.CT_JWKS_URL!,
-    jwtIssuer: process.env.CT_JWT_ISSUER!,
-  });
-  const client = sdk.client.ctAPI.client;
-  try {
-    const response = await client.types().withKey({ key: 'ingrid-session-id' }).get().execute();
-
-    if (response.statusCode !== 200) {
-      console.info('Ingrid custom type does not exist, creating it');
-      try {
-        const res = await client
-          .types()
-          .post({
-            body: {
-              key: 'ingrid-session-id',
-              name: {
-                en: 'Ingrid Session ID',
-              },
-              resourceTypeIds: ['cart'],
-              fieldDefinitions: [
-                {
-                  name: 'ingridSessionId',
-                  label: {
-                    en: 'Ingrid Session ID',
-                  },
-                  type: {
-                    name: 'String',
-                  },
-                  required: false,
-                },
-              ],
-            },
-          })
-          .execute();
-        const curstomType = res.body;
-        console.log('Ingrid custom type created', curstomType);
-      } catch (error) {
-        console.error('Error creating Ingrid custom type', error);
-      }
-    }
-    const customType = response.body;
-    console.log('Ingrid custom type created', customType);
-  } catch (error) {
-    console.error('Error checking if Ingrid custom type exists', error);
-  }
-  return;
-} */
 
 run();
