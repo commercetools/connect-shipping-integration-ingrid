296c36332e33eb6af34cbc90ed43564c30d8a0cf
James Morrison <james@frontastic.cloud>
2025-02-27 10:18:26 +0000

    chore: refactor and rename logger filers

--- processor/src/libs/logger/index.ts
+++ processor/src/libs/logger/index.ts
@@ -1,41 +1,4 @@
-import { createApplicationLogger } from '@commercetools-backend/loggers';
-import { getRequestContext } from '../fastify/context/context';
-import { getConfig } from '../../config';
-import setIn from 'lodash/set';
-import { format } from 'logform';
-import cloneDeep from 'lodash/cloneDeep';
-import type { DefaultFields } from './types/logger.type';
+import { appLogger } from './appLogger';
+import { DefaultFields } from './types/DefaultFields.type';
 
-const defaultFieldsFormatter = (defaults: DefaultFields) => {
-  return format((info) => {
-    const clone = cloneDeep(info);
-
-    Object.entries(defaults).forEach(([key, value]) => {
-      if (value === undefined) {
-        // Do nothing
-      } else if (typeof value === 'object' || typeof value === 'string') {
-        setIn(clone, key, value);
-      } else if (typeof value === 'function') {
-        const evaluated = value();
-        if (evaluated !== undefined) {
-          setIn(clone, key, evaluated);
-        }
-      }
-    });
-
-    return clone;
-  })(defaults);
-};
-
-export const appLogger = createApplicationLogger({
-  formatters: [
-    defaultFieldsFormatter({
-      projectKey: getConfig().projectKey,
-      version: process.env.npm_package_version,
-      name: process.env.npm_package_name,
-      correlationId: () => getRequestContext().correlationId,
-      pathTemplate: () => getRequestContext().pathTemplate,
-      path: () => getRequestContext().path,
-    }),
-  ],
-});
+export { appLogger, type DefaultFields };
