705cf14f4ad44d5e3bd11c58692346eacc8ce878
Marius <73521657+mariusmhm@users.noreply.github.com>
2025-03-06 17:36:58 +0100

    fix(event-handler): removed unusedVars & prettier change

--- event-handler/src/middleware/error.middleware.ts
+++ event-handler/src/middleware/error.middleware.ts
@@ -1,12 +1,11 @@
-import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
+import { ErrorRequestHandler, Request, Response } from 'express';
 import CustomError from '../errors/custom.error';
 import { logger } from '../utils/logger.utils';
 
 export const errorMiddleware: ErrorRequestHandler = (
   error: Error,
   _: Request,
-  res: Response,
-  _next: NextFunction
+  res: Response
 ) => {
   const isDevelopment = process.env.NODE_ENV === 'development';
 
