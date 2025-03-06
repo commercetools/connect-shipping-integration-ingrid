94839cda6a13c465f1d491ee644b6569b22ccfb3
Marius <73521657+mariusmhm@users.noreply.github.com>
2025-03-06 14:43:47 +0100

    fix(event-handler): disabled linting on tests and fixed linting errors

--- event-handler/.eslintrc.cjs
+++ event-handler/.eslintrc.cjs
@@ -17,4 +17,20 @@ module.exports = {
     jest: true,
     node: true, //adds things like process to global
   },
+  overrides: [
+    {
+      files: ['tests/**/*.ts', 'tests/**/*.js'],
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        '@typescript-eslint/ban-ts-comment': [
+          'error',
+          {
+            'ts-expect-error': 'allow-with-description',
+            'ts-ignore': false,
+          },
+        ],
+      },
+    },
+  ],
 };
