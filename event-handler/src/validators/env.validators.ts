adb5ea791087b56f34adeeea3f3b2e60571ba9ca
King-Hin Leung <kinghin.leung@commercetools.com>
2025-04-03 16:21:15 +0200

    fix(event-handler): remove optional function in env.validator

--- event-handler/src/validators/env.validators.ts
+++ event-handler/src/validators/env.validators.ts
@@ -1,5 +1,5 @@
+
 import {
-  optional,
   standardString,
   standardKey,
   region,
@@ -8,49 +8,53 @@ import {
 /**
  * Create here your own validators
  */
+
+const clientIdValidator : [string[], [[(o: object) => boolean, string, [object]]]] = standardString(
+  ['clientId'],
+  {
+    code: 'InValidClientId',
+    message: 'Client id should be 24 characters.',
+    referencedBy: 'environmentVariables',
+  },
+  { min: 24, max: 24 }
+)
+const clientSecretValidator : [string[], [[(o: object) => boolean, string, [object]]]] = standardString(
+  ['clientSecret'],
+  {
+    code: 'InvalidClientSecret',
+    message: 'Client secret should be 32 characters.',
+    referencedBy: 'environmentVariables',
+  },
+  { min: 32, max: 32 }
+)
+const projectKeyValidator = standardKey(['projectKey'], {
+  code: 'InvalidProjectKey',
+  message: 'Project key should be a valid string.',
+  referencedBy: 'environmentVariables',
+})
+const regionValidator = region(['region'], {
+  code: 'InvalidRegion',
+  message: 'Not a valid region.',
+  referencedBy: 'environmentVariables',
+})
+
+const scopeValidator = standardString(
+  ['scope'],
+  {
+    code: 'InvalidScope',
+    message: 'Scope should be at least 2 characters long.',
+    referencedBy: 'environmentVariables',
+  },
+  { min: 2, max: undefined }
+)
+
 const envValidators = [
-  standardString(
-    ['clientId'],
-    {
-      code: 'InValidClientId',
-      message: 'Client id should be 24 characters.',
-      referencedBy: 'environmentVariables',
-    },
-    { min: 24, max: 24 }
-  ),
-
-  standardString(
-    ['clientSecret'],
-    {
-      code: 'InvalidClientSecret',
-      message: 'Client secret should be 32 characters.',
-      referencedBy: 'environmentVariables',
-    },
-    { min: 32, max: 32 }
-  ),
-
-  standardKey(['projectKey'], {
-    code: 'InvalidProjectKey',
-    message: 'Project key should be a valid string.',
-    referencedBy: 'environmentVariables',
-  }),
-  optional(
-    standardString)(
-      ['scope'],
-      {
-        code: 'InvalidScope',
-        message: 'Scope should be at least 2 characters long.',
-        referencedBy: 'environmentVariables',
-      },
-      { min: 2, max: undefined }
-    
-  ),
-
-  region(['region'], {
-    code: 'InvalidRegion',
-    message: 'Not a valid region.',
-    referencedBy: 'environmentVariables',
-  }),
+  clientIdValidator,
+  clientSecretValidator,
+  projectKeyValidator,
+  regionValidator,
+  scopeValidator
+  
 ];
 
 export default envValidators;
